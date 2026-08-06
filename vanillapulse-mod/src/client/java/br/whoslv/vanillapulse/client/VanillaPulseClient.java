04caa212640ddb288f520f1b9034317a6e1d9d50
Vitor Cesar <vitorcesarsouza7@gmail.com>
2026-08-06 14:08:14 -0300

    feat: initialize Fabric project

---
+++ vanillapulse-mod/src/client/java/br/whoslv/vanillapulse/client/VanillaPulseClient.java
@@ -0,0 +1,10 @@
+package br.whoslv.vanillapulse.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaPulseClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
